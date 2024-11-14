f81980261e201734115dad24fca18a208db91a01
HyoJeong Hong <137891816+hyojeong0407@users.noreply.github.com>
2024-11-15 00:18:59 +0900

    [Bronze III] Title: 최소, 최대, Time: 176 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, num, min = 1000001, max = -1000001;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++){
+        scanf("%d", &num);
+        if (num > max)
+            max = num;
+        if (num < min)
+            min = num;
+    }
+    printf("%d %d", min, max);
+	return 0;
+}
