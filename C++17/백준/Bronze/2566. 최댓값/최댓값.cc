2e9b48808b635f7d95353b379a48c4f8a154fa71
HyoJeong Hong <137891816+hyojeong0407@users.noreply.github.com>
2024-11-14 22:46:37 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2566. 최댓값/최댓값.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int arr[9][9] = { 0 };
+	int max = 0;
+	int row = 1, col = 1;
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			scanf("%d", &arr[i][j]);
+			if (max < arr[i][j]) {
+				max = arr[i][j];
+				row = i + 1;
+				col = j + 1;
+			}
+		}
+	}
+
+	printf("%d\n%d %d", max, row, col);
+
+    return 0;
+}
