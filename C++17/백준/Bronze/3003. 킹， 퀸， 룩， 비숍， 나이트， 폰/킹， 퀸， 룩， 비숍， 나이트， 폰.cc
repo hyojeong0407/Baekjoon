ad00e9fc5e0a3544fb664e070de85cbb979ff495
HyoJeong Hong <137891816+hyojeong0407@users.noreply.github.com>
2024-11-15 00:02:48 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int K, Q, L, B, N, P;
+    scanf("%d %d %d %d %d %d", &K, &Q, &L, &B, &N, &P);
+
+    printf("%d %d %d %d %d %d\n", 1 - K, 1 - Q, 2 - L, 2 - B, 2 - N, 8 - P);
+
+    return 0;
+}
