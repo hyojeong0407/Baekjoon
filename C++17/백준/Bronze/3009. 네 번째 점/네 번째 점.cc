87cdd53523596b5fdb3bbc15bfe92deb289c0f77
HyoJeong Hong <137891816+hyojeong0407@users.noreply.github.com>
2024-11-15 00:03:08 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int x1, y1;
+    scanf("%d %d", &x1, &y1);
+    int x2, y2;
+    scanf("%d %d", &x2, &y2);
+    int x3, y3;
+    scanf("%d %d", &x3, &y3);
+
+    if (x1 == x2) {
+        printf("%d ", x3);
+    }
+    else if (x2 == x3) {
+        printf("%d ", x1);
+    }
+    else if (x1 == x3) {
+        printf("%d ", x2);
+    }
+
+    if (y1 == y2) {
+        printf("%d", y3);
+    }
+    else if (y2 == y3) {
+        printf("%d", y1);
+    }
+    else if (y1 == y3) {
+        printf("%d", y2);
+    }
+
+    return 0;
+}
